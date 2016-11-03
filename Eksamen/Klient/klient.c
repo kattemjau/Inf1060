9772be5ea142e8b1dd14cbf3e9c86eee6b98375a
kattemjau <sindre.c.h@hotmail.com>
2016-11-03 21:39:35 +0100

    server connect

--- Eksamen/Klient/klient.c
+++ Eksamen/Klient/klient.c
@@ -1,13 +1,65 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
-//#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+
+//#include <netdb.h> get host by name
+// #include <unistd.h> pipe it up
 
 void myHandler(int s){
   printf("ctrl + c sensed\n");
-  exit(1);
+  exit(EXIT_FAILURE);
+}
+
+int create_socket(char *ip, char *port){
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  if(sock == -1){
+    perror("socket()");
+    return EXIT_FAILURE;
+  }
+
+  int portNb = atoi(port);
+
+  struct sockaddr_in serveraddr;
+  memset(&serveraddr, 0, sizeof(struct sockaddr_in));
+
+  serveraddr.sin_family=AF_INET;
+  serveraddr.sin_port=htons(portNb);
+
+  //Bruker inet_pton istedet for inet_aton grunnet den sjekker ipen opp mot adresse familien
+  int ine = inet_pton(AF_INET, ip, &serveraddr.sin_addr);
+
+  //Sjekker om inet_pton returnerer noe annet enn 1
+  if(ine == 0){
+    fprintf(stderr, "Not a valid ip adress: %s\n", ip );
+
+  }else if(ine == -1){
+    perror("inet_pton()");
+    close(sock);
+    return EXIT_FAILURE;
+  }
+
+  printf("Connecting to ip: %s:%s\n", ip, port );
+
+  // Prover a koble til server
+  if(connect(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr))){
+    //returner 1 hvis ikke klarer a connecte
+    fprintf(stderr, "Couldent connect to server: %son:%s\n", ip, port);
+    perror("connect()");
+    close(sock);
+    return EXIT_FAILURE;
+  }
+
+  printf("Connected to server: %s\n", ip);
+  //hva som skal skje etter klient er koblet til server:
+
+  //signal handler for ctrl + c from signal.h
+  // lage en handler for signal, som kjorer en selvlaget exception
+
+  return sock;
 }
 
 
@@ -29,52 +81,12 @@ int main(int argc, char const *argv[]) {
   sigaction(SIGINT, &sig, NULL);
 
   // lager ny socket
-  sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
+  sock = create_socket(ip, port);
   if(sock == -1){
-    fprintf(stderr, "An socket error has accured\n" );
-    perror("socket()");
     exit(EXIT_FAILURE);
   }
-
-  int portNb = atoi(port);
-
-  struct sockaddr_in serveraddr;
-  memset(&serveraddr, 0, sizeof(struct sockaddr_in));
-
-  serveraddr.sin_family=AF_INET;
-  serveraddr.sin_port=htons(portNb);
-
-  //Bruker inet_pton istedet for inet_aton grunnet den sjekker ipen opp mot adresse familien
-  int ine = inet_pton(AF_INET, ip, &serveraddr.sin_addr);
-
-  //Sjekker om inet_pton returnerer noe annet enn 1
-  if(ine == 0){
-    fprintf(stderr, "Not a valid ip adress: %s\n", ip );
-
-  }else if(ine == -1){
-    perror("inet_pton()");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-
-  printf("Connecting to ip: %s:%s\n", ip, port );
-
-  // Prover a koble til server
-  if(connect(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr))){
-    //returner 1 hvis ikke klarer a connecte
-  	fprintf(stderr, "Couldent connect to server: %son:%s\n", ip, port);
-    perror("connect()");
-  	close(sock);
-  	exit(EXIT_FAILURE);
-  }
-
-  printf("Connected to server: %s\n", ip);
-  //hva som skal skje etter klient er koblet til server:
-
-  //signal handler for ctrl + c from signal.h
-  // lage en handler for signal, som kjorer en selvlaget exception
-
+  //lage barn og pipes
+  
   printf("Terminating program\n");
   close(sock);
   return 0;
--- Eksamen/Server/server.c
+++ Eksamen/Server/server.c
@@ -6,6 +6,62 @@
 #include <string.h>
 #include <signal.h>
 
+void myHandler(int s){
+  printf("ctrl + c sensed\n");
+  exit(EXIT_FAILURE);
+
+}
+
+int create_socket(char *port){
+
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(sock == -1){
+    perror("socket()");
+    return EXIT_FAILURE;
+  }
+
+  int portNb = atoi(port);
+  
+  struct sockaddr_in serveraddr;
+  memset(&sockaddr_in, 0, sizeof(struct sockaddr_in));
+  // server_addr.sin_family = AF_INET;
+  server_addr.sin_port=htons(portNb);
+
+  // har ikke server sin ip
+  // TODO: legge til &server_addr.sin_addr.s_addr ??
+  /*
+  int pton = inet_pton(AF_INET, ip, &server_addr.sin_addr);
+
+  if(pton == -1){
+    perror("inet_pton()");
+    close(sock);
+    return EXIT_FAILURE;
+  } else if(pton == 0){
+      fprintf(stderr, "inet_pton family errror\n");
+    close(sock);
+    return EXIT_FAILURE;
+  } */
+
+
+  if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))){
+    perror("setsockopt()");
+    close(sock);
+    return EXIT_FAILURE;
+  }
+
+  if(bind(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr))){
+    perror("bind()");
+    close(sock);
+    return EXIT_FAILURE;
+  }
+
+  printf("Sucessfully binded to port: %s\n", port);
+
+  return sock;
+
+
+}
+
 int main(int argc, char const *argv[]) {
 
   if(argc != 3){
@@ -17,13 +73,19 @@ int main(int argc, char const *argv[]) {
   char *job=argv[1];
   char *port=argv[2];
 
-  struct sigaction action;
-  memset(&action, 0, sizeof(action));
+  //ctrl + c handling
+  struct sigaction sig;
+  // memset(&sig, 0, sizeof(sig));
+  sa.sa_handler = myHandler;
+  sigaction(SIGINT, &sa, NULL);
+
+  int sock = create_socket(port);
+
+  if(sock ){
+
+  }
 
 
-
-
-
-
+  close(sock);
   return 0;
 }
