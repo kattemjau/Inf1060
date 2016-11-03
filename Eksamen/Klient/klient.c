a803aeaecbb7725480fb3b22334b16e26b325ffa
kattemjau <sindre.c.h@hotmail.com>
2016-11-03 19:22:47 +0100

    added ctrl + c detection

--- Eksamen/Klient/klient.c
+++ Eksamen/Klient/klient.c
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+void myHandler(int s){
+  printf("ctrl + c sensed\n");
+  exit(1);
+}
+
+
 int main(int argc, char const *argv[]) {
 
   if(argc != 3){
@@ -15,6 +21,12 @@ int main(int argc, char const *argv[]) {
   int sock;
   char *ip=argv[1];
   char *port=argv[2];
+
+  //lager en ctrl + c signal behandler
+  struct sigaction sig;
+  dig.sa_handler = myHandler;
+
+  sigaction(SIGINT, &sig, NULL);
 
   // lager ny socket
   sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -41,22 +53,29 @@ int main(int argc, char const *argv[]) {
     fprintf(stderr, "Not a valid ip adress: %s\n", ip );
 
   }else if(ine == -1){
-    perror("connect()");
+    perror("inet_pton()");
     close(sock);
     exit(EXIT_FAILURE);
   }
 
   printf("Connecting to ip: %s:%s\n", ip, port );
 
+  // Prover a koble til server
   if(connect(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr))){
     //returner 1 hvis ikke klarer a connecte
-
-
+  	fprintf(stderr, "Couldent connect to server: %son:%s\n", ip, port);
+    perror("connect()");
+  	close(sock);
+  	exit(EXIT_FAILURE);
   }
 
+  printf("Connected to server: %s\n", ip);
+  //hva som skal skje etter klient er koblet til server:
 
+  //signal handler for ctrl + c from signal.h
+  // lage en handler for signal, som kjorer en selvlaget exception
 
-
+  printf("Terminating program\n");
   close(sock);
   return 0;
 }
